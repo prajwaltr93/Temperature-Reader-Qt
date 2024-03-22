836e225a9ba0070213774658f3d0c89bee9769fa
Prajwal T R <prajwaltr93@gmail.com>
2024-03-22 15:45:58 +0530

    add: application main icon
    (●'◡'●)

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    a.setWindowIcon(QIcon(":/icons/resources/main-icon.png"));
+
     MainWidget w;
     w.show();
     return a.exec();
